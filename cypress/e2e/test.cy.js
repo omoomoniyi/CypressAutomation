c07b002e7f8029c6ac21f5a37dac02bbb5befa6a
Omoniyi Osadare <osadareomoniyi@gmail.com>
2024-06-27 02:32:17 +0100

    Assertion For All Test Cases Updated

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -10,18 +10,6 @@ it('Successful Login With Valid Credentials', () =>
         cy.wait(3000)
         cy.get('.wp-block-button__link').should('be.visible')
 
-
-
-        // cy.wait(6000)
-        // cy.get(':nth-child(2) > .form__input', { timeout: 20000 }).should('be.visible').type('omoniyitest')
-        // cy.wait(6000)
-        // cy.get(':nth-child(4) > .form__input').type('omoniyitest')
-        // cy.wait(6000)
-        // cy.get('.btn').click()
-        // cy.wait(20000)
-        // cy.get('.fetch-offline-data').should('be.visible')
-        
-
     
     })
 
@@ -29,12 +17,11 @@ it('Successful Login With Valid Credentials', () =>
     
         {
             cy.visit('https://practicetestautomation.com/practice-test-login/')
-            // cy.get(':nth-child(2) > .form__input').type('admintest')
-            // cy.get(':nth-child(4) > .form__input').type('1234567890')
-            // cy.get('.btn').click()
-            // cy.wait(2000)
-            // cy.contains('Invalid Login Id or Password').should('be.visible')
-            
+            cy.get('#username').type('incorrectUser ')
+            cy.get('#password').type('Password123')
+            cy.get('#submit').click()
+            cy.wait(3000)
+
     
         
         })
@@ -43,12 +30,12 @@ it('Successful Login With Valid Credentials', () =>
     
             {
                 cy.visit('https://practicetestautomation.com/practice-test-login/')
-                // cy.get(':nth-child(2) > .form__input').type('admintest')
-                // cy.get(':nth-child(4) > .form__input').type('1234567890')
-                // cy.get('.btn').click()
-                // cy.wait(2000)
-                // cy.contains('Invalid Login Id or Password').should('be.visible')
-                
+                cy.get('#username').type('incorrectUser ')
+                cy.get('#password').type('Password123')
+                cy.get('#submit').click()
+                cy.wait(3000)
+                cy.get('#error').should('be.visible')
+ 
         
             
             })
