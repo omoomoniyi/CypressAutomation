ab67095d666451c68bff81a1f1491d2d171a32d0
Omoniyi Osadare <osadareomoniyi@gmail.com>
2024-06-27 01:58:40 +0100

    Increased defaultCommandTimeout

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   e2e: {
 
     watchForFileChanges: false,
-    defaultCommandTimeout: 3000,
+    defaultCommandTimeout: 10000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
