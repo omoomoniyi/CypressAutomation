5e8ee4bee9ca4ef5b516a4649af15faa27007f9d
Omoniyi Osadare <osadareomoniyi@gmail.com>
2024-06-27 02:18:47 +0100

    New Test URL

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -3,15 +3,20 @@
 it('Successful Login With Valid Credentials', () =>
     
     {
-        cy.visit('https://customer-code.surge.sh/#/')
-        cy.wait(6000)
-        cy.get(':nth-child(2) > .form__input', { timeout: 20000 }).should('be.visible').type('omoniyitest')
-        cy.wait(6000)
-        cy.get(':nth-child(4) > .form__input').type('omoniyitest')
-        cy.wait(6000)
-        cy.get('.btn').click()
-        cy.wait(20000)
-        cy.get('.fetch-offline-data').should('be.visible')
+        cy.visit('https://practicetestautomation.com/practice-test-login/')
+        cy.get('#username').type('student')
+        cy.get('#password').type('Password123')
+        cy.get('#submit').click()
+
+
+        // cy.wait(6000)
+        // cy.get(':nth-child(2) > .form__input', { timeout: 20000 }).should('be.visible').type('omoniyitest')
+        // cy.wait(6000)
+        // cy.get(':nth-child(4) > .form__input').type('omoniyitest')
+        // cy.wait(6000)
+        // cy.get('.btn').click()
+        // cy.wait(20000)
+        // cy.get('.fetch-offline-data').should('be.visible')
         
 
     
@@ -20,12 +25,12 @@ it('Successful Login With Valid Credentials', () =>
     it('Failed Login With Invalid Credentials', () =>
     
         {
-            cy.visit('https://customer-code.surge.sh/#/')
-            cy.get(':nth-child(2) > .form__input').type('admintest')
-            cy.get(':nth-child(4) > .form__input').type('1234567890')
-            cy.get('.btn').click()
-            cy.wait(2000)
-            cy.contains('Invalid Login Id or Password').should('be.visible')
+            cy.visit('https://practicetestautomation.com/practice-test-login/')
+            // cy.get(':nth-child(2) > .form__input').type('admintest')
+            // cy.get(':nth-child(4) > .form__input').type('1234567890')
+            // cy.get('.btn').click()
+            // cy.wait(2000)
+            // cy.contains('Invalid Login Id or Password').should('be.visible')
             
     
         
@@ -34,12 +39,12 @@ it('Successful Login With Valid Credentials', () =>
         it('Error Message Display When Login Fails', () =>
     
             {
-                cy.visit('https://customer-code.surge.sh/#/')
-                cy.get(':nth-child(2) > .form__input').type('admintest')
-                cy.get(':nth-child(4) > .form__input').type('1234567890')
-                cy.get('.btn').click()
-                cy.wait(2000)
-                cy.contains('Invalid Login Id or Password').should('be.visible')
+                cy.visit('https://practicetestautomation.com/practice-test-login/')
+                // cy.get(':nth-child(2) > .form__input').type('admintest')
+                // cy.get(':nth-child(4) > .form__input').type('1234567890')
+                // cy.get('.btn').click()
+                // cy.wait(2000)
+                // cy.contains('Invalid Login Id or Password').should('be.visible')
                 
         
             
